47e8aa3e5d87ce2f2ac86ffcc32dda76f5e5f157
Pollyana Valverde <valverdepolly@gmail.com>
2026-01-28 10:48:06 -0300

    feat: add TasksSummary component to Home page for task overview

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,5 +1,13 @@
 import { Container } from "../components/Container";
 
+import { TasksSummary } from "../core-components/TasksSummary";
+
 export function Home() {
-  return <Container>Home Page</Container>;
+  return (
+    <Container as="article" className="space-y-3">
+      <header className="flex items-center justify-between">
+        <TasksSummary />
+      </header>
+    </Container>
+  );
 }
