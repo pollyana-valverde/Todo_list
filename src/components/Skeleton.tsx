d353ce8fb2103355172b06410a7b3bfccb83b309
Pollyana Valverde <valverdepolly@gmail.com>
2026-01-28 09:36:07 -0300

    feat: add Skeleton component with customizable rounded variants

---
+++ src/components/Skeleton.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const skeletonVariants = cva("animate-pulse bg-gray-200",
+    {
+        variants: {
+            rounded: {
+                sm: "rounded-sm",
+                lg: "rounded-lg",
+                full: "rounded-full",
+            },
+        },
+        defaultVariants: {
+            rounded: "lg",
+        }
+    }
+)
+
+
+interface SkeletonProps extends React.ComponentProps<"div">, VariantProps<typeof skeletonVariants> {}
+
+export function Skeleton ({
+    rounded,
+    className,
+    ...rest
+}: SkeletonProps
+) {
+    return (
+        <div
+            className={skeletonVariants({ rounded, className })}
+            {...rest}
+        />
+    );
+}
