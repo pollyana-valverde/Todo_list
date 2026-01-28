64f9eecec20a2913029a91b3f9187b870197220a
Pollyana Valverde <valverdepolly@gmail.com>
2026-01-28 09:29:27 -0300

    feat: add Container component with customizable size variants

---
+++ src/components/Container.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const containerVariants = cva("mx-auto", {
+    variants: {
+        size: {
+            md: "max-w-126 px-2",
+        },
+    },
+    defaultVariants: {
+        size: "md",
+    },
+})
+
+interface ContainerProps extends React.ComponentProps<"div">, VariantProps<typeof containerVariants> {
+    as?: keyof React.JSX.IntrinsicElements;
+}
+
+export function Container ({
+    as = "div",
+    size,
+    className,
+    children,
+    ...rest
+}: ContainerProps
+) {
+    return React.createElement(
+        as,
+        {
+            className: containerVariants({ size, className }),
+            ...rest,
+        },
+        children
+    );
+}
