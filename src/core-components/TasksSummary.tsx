b2ae21c3e60c2e0c644550e1117159d160407804
Pollyana Valverde <valverdepolly@gmail.com>
2026-01-28 19:59:47 -0300

    feat: add loading state to badges in TasksSummary component

--- src/core-components/TasksSummary.tsx
+++ src/core-components/TasksSummary.tsx
@@ -4,17 +4,25 @@ import { Text } from "../components/Text";
 import { useTasks } from "../hooks";
 
 export function TasksSummary() {
-const { createdTasksCount, concludedTasksCount} = useTasks();
+  const { createdTasksCount, concludedTasksCount, isLoadingTasks } = useTasks();
 
   return (
     <React.Fragment>
       <div className="flex items-center gap-2">
-        <Text variant="body-sm-bold" className="text-gray-300!">Tarefas criadas</Text>
-        <Badge variant="secondary">{createdTasksCount}</Badge>
+        <Text variant="body-sm-bold" className="text-gray-300!">
+          Tarefas criadas
+        </Text>
+        <Badge variant="secondary" isLoading={isLoadingTasks}>
+          {createdTasksCount}
+        </Badge>
       </div>
       <div className="flex items-center gap-2">
-        <Text variant="body-sm-bold" className="text-gray-300!">Concluídas</Text>
-        <Badge variant="primary">{concludedTasksCount} de {createdTasksCount}</Badge>
+        <Text variant="body-sm-bold" className="text-gray-300!">
+          Concluídas
+        </Text>
+        <Badge variant="primary" isLoading={isLoadingTasks}>
+          {concludedTasksCount} de {createdTasksCount}
+        </Badge>
       </div>
     </React.Fragment>
   );
